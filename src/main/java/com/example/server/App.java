bccbf0e818f71d0a2ad14bc24fee88a04359ceb9
Ho Yan Leung <hy.leung@gmail.com>
2016-04-13 05:08:29 -0700

    Moves bind of tracer into module

--- src/main/java/com/example/server/App.java
+++ src/main/java/com/example/server/App.java
@@ -2,8 +2,6 @@ package com.example.server;
 
 import com.github.kristofa.brave.Brave;
 import ratpack.server.RatpackServer;
-import ratpack.zipkin.ServerTracingDecorator;
-import ratpack.zipkin.ServerTracingHandler;
 import ratpack.zipkin.ServerTracingModule;
 
 import static ratpack.guice.Guice.registry;
@@ -18,11 +16,11 @@ public class App {
         server
             .serverConfig(config -> config.port(serverPort))
             .registry(registry(binding ->
-                binding.bind(ServerTracingHandler.class)
-                       .bind(ServerTracingDecorator.class)
-                       .module(ServerTracingModule.class)
-                       .bindInstance(new ServerTracingModule.Config()
-                           .withBrave(new Brave.Builder("ratpack-demo").build()))
+                binding
+                    .module(ServerTracingModule.class)
+                    .bindInstance(ServerTracingModule.config()
+                                                     .withBrave(new Brave.Builder("ratpack-demo")
+                                                         .build()))
             ))
             .handlers(handler ->
                 handler.all(ctx -> ctx.render("Hello world"))
--- src/main/java/ratpack/zipkin/ServerTracingModule.java
+++ src/main/java/ratpack/zipkin/ServerTracingModule.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ratpack.zipkin;
 
 import com.github.kristofa.brave.*;
@@ -7,13 +22,13 @@ import com.google.inject.Provides;
 import ratpack.guice.ConfigurableModule;
 
 /**
- * Created by hyleung on 2016-04-03.
+ * Module for ZipKin distributed tracing.
  */
 public class ServerTracingModule extends ConfigurableModule<ServerTracingModule.Config> {
   @Override
   protected void configure() {
-
-
+    bind(ServerTracingHandler.class);
+    bind(ServerTracingDecorator.class);
   }
 
   @Provides
@@ -46,9 +61,16 @@ public class ServerTracingModule extends ConfigurableModule<ServerTracingModule.
     return new DefaultSpanNameProvider();
   }
 
+  public static Config config() {
+    return new Config();
+  }
+
   public static class Config {
     private Brave brave;
 
+    private Config() {
+      //no-op
+    }
     public Config withBrave(final Brave brave) {
       this.brave = brave;
       return this;
