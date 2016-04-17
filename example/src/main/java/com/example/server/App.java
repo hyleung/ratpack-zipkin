4251ce14023887784ef335078d41d780e4c0e56c
Ho Yan Leung <hy.leung@gmail.com>
2016-04-17 11:17:28 -0700

    Adds scribe collector to example app

--- example/src/main/java/com/example/server/App.java
+++ example/src/main/java/com/example/server/App.java
@@ -2,6 +2,7 @@ package com.example.server;
 
 import com.github.kristofa.brave.Brave;
 import com.github.kristofa.brave.KeyValueAnnotation;
+import com.github.kristofa.brave.scribe.ScribeSpanCollector;
 import com.google.common.collect.Lists;
 import ratpack.server.RatpackServer;
 import ratpack.zipkin.ServerTracingModule;
@@ -14,23 +15,28 @@ import static ratpack.guice.Guice.registry;
 public class App {
   public static void main(String[] args) throws Exception {
     Integer serverPort = Integer.parseInt(System.getProperty("port", "8080"));
+    String scribeHost = System.getProperty("scribeHost");
+    Brave.Builder builder = new Brave.Builder("ratpack-demo");
+    if (scribeHost != null) {
+      builder.spanCollector(new ScribeSpanCollector(scribeHost, 9410));
+    }
     RatpackServer.start(server ->
         server
             .serverConfig(config -> config.port(serverPort))
-            .registry(registry(binding ->
-                binding
-                    .module(ServerTracingModule.class)
-                    .bind(HelloWorldHandler.class)
-                    .bindInstance(ServerTracingModule
-                        .config()
-                        .withBrave(new Brave.Builder("ratpack-demo")
-                            .build())
-                        .withRequestAnnotations(request ->
-                            Lists.newArrayList(KeyValueAnnotation.create("uri", request.getUri()))
-                        )
-                        .withResponseAnnotations(response ->
-                            Lists.newArrayList(KeyValueAnnotation.create("foo","bar"))
-                        ))
+            .registry(registry(binding -> {
+                  binding
+                      .module(ServerTracingModule.class)
+                      .bind(HelloWorldHandler.class)
+                      .bindInstance(ServerTracingModule
+                          .config()
+                          .withBrave(builder.build())
+                          .withRequestAnnotations(request ->
+                              Lists.newArrayList(KeyValueAnnotation.create("uri", request.getUri()))
+                          )
+                          .withResponseAnnotations(response ->
+                              Lists.newArrayList(KeyValueAnnotation.create("foo", "bar"))
+                          ));
+                }
             ))
             .handlers(handler ->
                 handler
