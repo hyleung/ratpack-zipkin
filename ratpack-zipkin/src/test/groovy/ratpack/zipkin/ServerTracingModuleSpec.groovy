d3412b66e8206b12dcf20bac6471a38b82182296
Ho Yan Leung <hy.leung@gmail.com>
2016-04-22 06:50:20 -0700

    More module config tests

--- ratpack-zipkin/src/test/groovy/ratpack/zipkin/ServerTracingModuleSpec.groovy
+++ ratpack-zipkin/src/test/groovy/ratpack/zipkin/ServerTracingModuleSpec.groovy
@@ -15,6 +15,11 @@
  */
 package ratpack.zipkin
 
+import com.github.kristofa.brave.Sampler
+import com.github.kristofa.brave.SpanCollector
+import com.github.kristofa.brave.http.SpanNameProvider
+import com.twitter.zipkin.gen.Span
+import org.assertj.core.util.Lists
 import ratpack.groovy.test.embed.GroovyEmbeddedApp
 import ratpack.guice.Guice
 import spock.lang.Specification
@@ -31,6 +36,28 @@ class ServerTracingModuleSpec extends Specification {
         expect:
             app.test { t -> t.get()}
     }
+
+    def 'Should initialize with all the configs!'() {
+        given:
+            def app = GroovyEmbeddedApp.of { server ->
+                server.registry(Guice.registry { binding ->
+                    binding.module(ServerTracingModule.class, { config ->
+                        config
+                            .serviceName("some-name")
+                            .port(1234)
+                            .address(InetAddress.getLocalHost())
+                            .sampler(Sampler.create(1f))
+                            .spanCollector(Stub(SpanCollector))
+                            .spanNameProvider(Stub(SpanNameProvider))
+                            .requestAnnotations{request -> Lists.emptyList()}
+                            .responseAnnotations(){response -> Lists.emptyList()}
+                    })
+                }).handlers { chain -> chain.all { ctx -> ctx.render("foo") } }
+            }
+        expect:
+            app.test { t -> t.get()}
+    }
+
     def 'Should initialize with service name config'() {
         given:
         def app = GroovyEmbeddedApp.of { server ->
@@ -42,5 +69,41 @@ class ServerTracingModuleSpec extends Specification {
         }
         expect:
         app.test { t -> t.get()}
+    }
+    def 'Should collect spans with SpanCollector'() {
+        given:
+            def spanCollector = Mock(SpanCollector)
+            def app = GroovyEmbeddedApp.of { server ->
+                server.registry(Guice.registry { binding ->
+                    binding.module(ServerTracingModule.class, { config ->
+                        config
+                            .serviceName("embedded")
+                            .spanCollector(spanCollector)
+                    })
+                }).handlers { chain -> chain.all { ctx -> ctx.render("foo") } }
+            }
+        when:
+            app.test { t -> t.get()}
+        then:
+            1 * spanCollector.collect(_ as Span)
+    }
+
+    def 'Should not collect spans with SpanCollector with 0 pct. sampling'() {
+        given:
+            def spanCollector = Mock(SpanCollector)
+            def app = GroovyEmbeddedApp.of { server ->
+                server.registry(Guice.registry { binding ->
+                    binding.module(ServerTracingModule.class, { config ->
+                        config
+                            .serviceName("embedded")
+                            .spanCollector(spanCollector)
+                            .sampler(Sampler.create(0))
+                })
+            }).handlers { chain -> chain.all { ctx -> ctx.render("foo") } }
+        }
+        when:
+            app.test { t -> t.get()}
+        then:
+            0 * spanCollector.collect(_ as Span)
     }
 }
