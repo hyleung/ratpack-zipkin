e4edf8619ce0272d9ec0d8054fee13b7114c1f31
Ho Yan Leung <hy.leung@gmail.com>
2016-07-17 08:31:57 -0700

    Add Kafka span collector(?)

--- example/src/main/java/com/example/server/App.java
+++ example/src/main/java/com/example/server/App.java
@@ -1,13 +1,10 @@
 package com.example.server;
 
-import com.github.kristofa.brave.KeyValueAnnotation;
-import com.github.kristofa.brave.LoggingSpanCollector;
-import com.github.kristofa.brave.Sampler;
+import com.github.kristofa.brave.*;
+import com.github.kristofa.brave.kafka.KafkaSpanCollector;
 import com.github.kristofa.brave.scribe.ScribeSpanCollector;
 import com.google.common.collect.Lists;
-import org.slf4j.MDC;
 import ratpack.guice.Guice;
-import ratpack.handling.RequestId;
 import ratpack.logging.MDCInterceptor;
 import ratpack.server.RatpackServer;
 import ratpack.zipkin.ServerTracingModule;
@@ -18,21 +15,23 @@ import ratpack.zipkin.ServerTracingModule;
 public class App {
   public static void main(String[] args) throws Exception {
     Integer serverPort = Integer.parseInt(System.getProperty("port", "8081"));
-    String scribeHost = System.getProperty("scribeHost");
     Float samplingPct = Float.parseFloat(System.getProperty("samplingPct","1"));
+
     RatpackServer.start(server -> server
         .serverConfig(config -> config.port(serverPort))
         .registry(Guice.registry(binding -> binding
-            .module(ServerTracingModule.class, config -> config
-                .serviceName("ratpack-demo")
-                .sampler(Sampler.create(samplingPct))
-                .spanCollector(scribeHost != null ? new ScribeSpanCollector(scribeHost, 9410) : new LoggingSpanCollector())
-                .requestAnnotations(request ->
-                    Lists.newArrayList(KeyValueAnnotation.create("uri", request.getUri()))
-                )
-                .responseAnnotations(response ->
-                    Lists.newArrayList(KeyValueAnnotation.create("foo", "bar"))
-                ))
+            .module(ServerTracingModule.class, config -> {
+              config
+                  .serviceName("ratpack-demo")
+                  .sampler(Sampler.create(samplingPct))
+                  .spanCollector(spanCollector())
+                  .requestAnnotations(request ->
+                      Lists.newArrayList(KeyValueAnnotation.create("uri", request.getUri()))
+                  )
+                  .responseAnnotations(response ->
+                      Lists.newArrayList(KeyValueAnnotation.create("foo", "bar"))
+                  );
+            })
             .bind(HelloWorldHandler.class)
             .add(MDCInterceptor.instance())
         ))
@@ -47,13 +46,29 @@ public class App {
             .module(ServerTracingModule.class, config -> config
                 .serviceName("other-server")
                 .sampler(Sampler.create(samplingPct))
-                .spanCollector(scribeHost != null ? new ScribeSpanCollector(scribeHost, 9410) : new LoggingSpanCollector()))
+                .spanCollector(spanCollector()))
             .bind(HelloWorldHandler.class)
             .add(MDCInterceptor.instance())
         ))
         .handlers(chain -> chain
             .all(ctx -> ctx.render("root")))
     );
+  }
+
+  private static SpanCollector spanCollector() {
+    String scribeHost = System.getProperty("scribeHost");
+    String kafkaHost = System.getProperty("kafkaHost");
+    if (scribeHost != null) {
+      return new ScribeSpanCollector(scribeHost, 9410);
+    } else if (kafkaHost != null) {
+      KafkaSpanCollector.Config config = KafkaSpanCollector
+          .Config.builder(kafkaHost)
+                 .topic("zipkin")
+                 .build();
+      return KafkaSpanCollector.create(config, new EmptySpanCollectorMetricsHandler());
+    } else {
+      return new LoggingSpanCollector();
+    }
   }
 
 }
