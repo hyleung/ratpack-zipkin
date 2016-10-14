ec309f2265bba23475bfcab2a10b6a89a26aebd8
Ho Yan Leung <hy.leung@gmail.com>
2016-10-14 06:56:50 -0700

    v1.0.1 release
    Bumps Brave to 3.14.1.
    
    Ref. https://github.com/openzipkin/brave/pull/254

--- gradle/publish_artifacts.sh
+++ gradle/publish_artifacts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $TRAVIS_BUILD_DIR
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then 
+if [ "$TRAVIS_BRANCH" == "1.0.x" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then 
     echo "Publishing artifacts to Sonatype OSS..." 
     ./gradlew upload -Psigning.keyId=$SIGNING_KEY_ID -Psigning.password=$SIGNING_PASSWORD -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/gradle/secring.gpg
 else
