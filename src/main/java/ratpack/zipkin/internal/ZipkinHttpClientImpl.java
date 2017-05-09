19c75e8bf663c595cc0d821d1e8d4b99a349301b
Lance Linder <llinder@gmail.com>
2017-05-09 15:39:10 -0500

    Use RatpackTraceContext and ExecHarness for client tests

--- src/main/java/ratpack/zipkin/internal/ZipkinHttpClientImpl.java
+++ src/main/java/ratpack/zipkin/internal/ZipkinHttpClientImpl.java
@@ -16,17 +16,22 @@
 package ratpack.zipkin.internal;
 
 import brave.Span;
+import brave.Tracer;
 import brave.http.HttpClientHandler;
 import brave.http.HttpTracing;
+import brave.propagation.CurrentTraceContext;
 import brave.propagation.TraceContext;
 import io.netty.buffer.ByteBufAllocator;
 import java.net.URI;
 import java.time.Duration;
 import java.util.concurrent.atomic.AtomicReference;
 import javax.inject.Inject;
+import javax.net.ssl.SSLContext;
 import ratpack.exec.Promise;
 import ratpack.exec.Result;
 import ratpack.func.Action;
+import ratpack.func.Function;
+import ratpack.http.HttpMethod;
 import ratpack.http.MutableHeaders;
 import ratpack.http.client.HttpClient;
 import ratpack.http.client.ReceivedResponse;
@@ -39,14 +44,18 @@ import ratpack.http.client.StreamedResponse;
 public class ZipkinHttpClientImpl implements HttpClient {
 
     private final HttpClient delegate;
-    final HttpClientHandler<MethodCapturingRequestSpec, Integer> handler;
+    final HttpClientHandler<WrappedRequestSpec, Integer> handler;
     final TraceContext.Injector<MutableHeaders> injector;
+    final Tracer tracer;
+    final CurrentTraceContext context;
 
     @Inject
     public ZipkinHttpClientImpl(final HttpClient delegate, final HttpTracing httpTracing) {
         this.delegate = delegate;
         this.handler = HttpClientHandler.create(httpTracing, new HttpAdapter());
         this.injector = httpTracing.tracing().propagation().injector(MutableHeaders::set);
+        this.tracer = httpTracing.tracing().tracer();
+        this.context = httpTracing.tracing().currentTraceContext();
     }
 
     @Override
@@ -77,16 +86,18 @@ public class ZipkinHttpClientImpl implements HttpClient {
     @Override
     public Promise<ReceivedResponse> request(URI uri, Action<? super RequestSpec> action) {
         AtomicReference<Span> span = new AtomicReference<>();
+        TraceContext current = context.get();
         return delegate
-            .request(uri, actionWithSpan(action, span))
+            .request(uri, actionWithSpan(action, span, current))
             .wiretap(response -> responseWithSpan(response, span));
     }
 
     @Override
     public Promise<StreamedResponse> requestStream(URI uri, Action<? super RequestSpec> requestConfigurer) {
         AtomicReference<Span> span = new AtomicReference<>();
+        TraceContext current = context.get();
         return delegate
-            .requestStream(uri, actionWithSpan(requestConfigurer, span))
+            .requestStream(uri, actionWithSpan(requestConfigurer, span, current))
             .wiretap(response -> streamedResponseWithSpan(response, span));
     }
 
@@ -100,9 +111,9 @@ public class ZipkinHttpClientImpl implements HttpClient {
         return request(uri, requestConfigurer.prepend(RequestSpec::post));
     }
 
-    private Action<? super RequestSpec> actionWithSpan(final Action<? super RequestSpec> action, final AtomicReference<Span> span) {
+    private Action<? super RequestSpec> actionWithSpan(final Action<? super RequestSpec> action, final AtomicReference<Span> span, final TraceContext current) {
         return action.append(request -> {
-            MethodCapturingRequestSpec captor = new MethodCapturingRequestSpec(this, request, span);
+            WrappedRequestSpec captor = new WrappedRequestSpec(this, request, span);
             action.execute(captor);
         });
     }
@@ -110,36 +121,144 @@ public class ZipkinHttpClientImpl implements HttpClient {
     private void streamedResponseWithSpan(Result<StreamedResponse> response, AtomicReference<Span> ref) {
         Span span = ref.get();
         if (span == null) return;
-        handler.handleReceive(response.getValue().getStatusCode(), response.getThrowable(), span);
+
+        Integer statusCode = (response.isError() || response.getValue() == null)
+            ? null : response.getValue().getStatusCode();
+
+        handler.handleReceive(statusCode, response.getThrowable(), span);
     }
 
     private void responseWithSpan(Result<ReceivedResponse> response, AtomicReference<Span> ref) {
         Span span = ref.get();
         if (span == null) return;
-        handler.handleReceive(response.getValue().getStatusCode(), response.getThrowable(), span);
+
+        Integer statusCode = (response.isError() || response.getValue() == null)
+            ? null : response.getValue().getStatusCode();
+
+        handler.handleReceive(statusCode, response.getThrowable(), span);
     }
 
     static final class HttpAdapter
-        extends brave.http.HttpClientAdapter<MethodCapturingRequestSpec, Integer> {
+        extends brave.http.HttpClientAdapter<WrappedRequestSpec, Integer> {
 
-        @Override public String method(MethodCapturingRequestSpec request) {
+        @Override public String method(WrappedRequestSpec request) {
             return request.getCapturedMethod().getName();
         }
 
-        @Override public String path(MethodCapturingRequestSpec request) {
+        @Override public String path(WrappedRequestSpec request) {
             return request.getUri().getPath();
         }
 
-        @Override public String url(MethodCapturingRequestSpec request) {
+        @Override public String url(WrappedRequestSpec request) {
             return request.getUri().toString();
         }
 
-        @Override public String requestHeader(MethodCapturingRequestSpec request, String name) {
+        @Override public String requestHeader(WrappedRequestSpec request, String name) {
             return request.getHeaders().get(name);
         }
 
         @Override public Integer statusCode(Integer response) {
             return response;
         }
+    }
+
+    /**
+     * "Dummy" implementation of RequestSpec, used to capture the HTTP request method.
+     *
+     */
+    static final class WrappedRequestSpec implements RequestSpec {
+
+        private final RequestSpec delegate;
+        private final ZipkinHttpClientImpl client;
+        private final AtomicReference<Span> span;
+        private HttpMethod capturedMethod;
+
+        WrappedRequestSpec(ZipkinHttpClientImpl client, RequestSpec spec,  AtomicReference<Span> span) {
+            this.delegate = spec;
+            this.client = client;
+            this.span = span;
+        }
+
+        @Override
+        public RequestSpec redirects(int maxRedirects) {
+            this.delegate.redirects(maxRedirects);
+            return this;
+        }
+
+        @Override
+        public RequestSpec onRedirect(Function<? super ReceivedResponse, Action<? super RequestSpec>> function) {
+            this.delegate.onRedirect(function);
+            return this;
+        }
+
+        @Override
+        public RequestSpec sslContext(SSLContext sslContext) {
+            this.delegate.sslContext(sslContext);
+            return this;
+        }
+
+        @Override
+        public MutableHeaders getHeaders() {
+            return this.delegate.getHeaders();
+        }
+
+        @Override
+        public RequestSpec maxContentLength(int numBytes) {
+            this.delegate.maxContentLength(numBytes);
+            return this;
+        }
+
+        @Override
+        public RequestSpec headers(Action<? super MutableHeaders> action) throws Exception {
+            this.delegate.headers(action);
+            return this;
+        }
+
+        @Override
+        public RequestSpec method(HttpMethod method) {
+            this.capturedMethod = method;
+            span.set(client.handler.handleSend(client.injector, getHeaders(), this));
+            this.delegate.method(method);
+            return this;
+        }
+
+        @Override
+        public RequestSpec decompressResponse(boolean shouldDecompress) {
+            this.delegate.decompressResponse(shouldDecompress);
+            return this;
+        }
+
+        @Override
+        public URI getUri() {
+            return this.delegate.getUri();
+        }
+
+        @Override
+        public RequestSpec connectTimeout(Duration duration) {
+            this.delegate.connectTimeout(duration);
+            return this;
+        }
+
+        @Override
+        public RequestSpec readTimeout(Duration duration) {
+            this.delegate.readTimeout(duration);
+            return this;
+        }
+
+        @Override
+        public Body getBody() {
+            return this.delegate.getBody();
+        }
+
+        @Override
+        public RequestSpec body(Action<? super Body> action) throws Exception {
+            this.delegate.body(action);
+            return this;
+        }
+
+        public HttpMethod getCapturedMethod() {
+            return capturedMethod;
+        }
+
     }
 }
---
+++ src/test/java/brave/http/ITTracingFeature_Client.java
@@ -0,0 +1,96 @@
+package brave.http;
+
+import brave.sampler.Sampler;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import java.io.IOException;
+import java.net.URI;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ratpack.exec.Execution;
+import ratpack.http.client.HttpClient;
+import ratpack.server.ServerConfig;
+import ratpack.test.exec.ExecHarness;
+import ratpack.util.Exceptions;
+import ratpack.zipkin.internal.RatpackCurrentTraceContext;
+import ratpack.zipkin.internal.ZipkinHttpClientImpl;
+
+public class ITTracingFeature_Client extends ITHttpClient<HttpClient> {
+
+    private static ExecHarness harness;
+
+    @BeforeClass
+    public static void beforeAll() {
+        harness = ExecHarness.harness();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        harness.close();
+    }
+
+    void harnessSetup(Execution execution) {
+        currentTraceContext = new RatpackCurrentTraceContext(() -> execution);
+        httpTracing = HttpTracing.create(tracingBuilder(Sampler.ALWAYS_SAMPLE).build());
+        client = newClient(server.getPort());
+    }
+
+    @Override protected HttpClient newClient(int port) {
+        return Exceptions.uncheck(() -> new ZipkinHttpClientImpl( HttpClient.of(s -> s
+            .poolSize(0)
+            .byteBufAllocator(UnpooledByteBufAllocator.DEFAULT)
+            .maxContentLength(ServerConfig.DEFAULT_MAX_CONTENT_LENGTH)
+        ), httpTracing));
+    }
+
+    @Override protected void closeClient(HttpClient client) throws IOException {
+        client.close();
+    }
+
+    @Override protected void get(HttpClient client, String pathIncludingQuery) throws Exception {
+        harness.yield(e -> client.get(URI.create(url(pathIncludingQuery)))).getValueOrThrow();
+    }
+
+    @Override protected void post(HttpClient client, String pathIncludingQuery, String body)
+        throws Exception {
+        harness.yield(e ->
+            client.post(URI.create(url(pathIncludingQuery)), (request ->
+                request.body(b -> b.text(body))
+            ))
+        ).getValueOrThrow();
+    }
+
+    @Override protected void getAsync(HttpClient client, String pathIncludingQuery)
+        throws Exception {
+        harness.yield(e -> client.get(URI.create(url(pathIncludingQuery)))).getValueOrThrow();
+    }
+
+    @Test
+    @Override public void reportsServerAddress() throws Exception {
+        // current implementation doesn't parse the server address
+    }
+
+    @Test
+    @Override public void makesChildOfCurrentSpan() throws Exception {
+        harness.run( e -> {
+            harnessSetup(e);
+            super.makesChildOfCurrentSpan();
+        });
+    }
+
+    @Test
+    @Override public void redirect() throws Exception {
+        harness.run( e -> {
+            harnessSetup(e);
+            super.redirect();
+        });
+    }
+
+    @Test
+    @Override public void usesParentFromInvocationTime() throws Exception {
+        harness.run( e -> {
+            harnessSetup(e);
+            super.usesParentFromInvocationTime();
+        });
+    }
+}
