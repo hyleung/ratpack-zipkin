cf3f661dfb59a9132a24babb7deeb983f191dd52
Ho Yan Leung <hy.leung@gmail.com>
2017-05-29 20:16:31 -0700

    Make TracedParallelBatch factory methods public.

--- src/main/java/ratpack/zipkin/TracedParallelBatch.java
+++ src/main/java/ratpack/zipkin/TracedParallelBatch.java
@@ -6,6 +6,16 @@ import ratpack.exec.Promise;
 import ratpack.exec.util.ParallelBatch;
 import ratpack.zipkin.internal.RatpackCurrentTraceContext;
 
+/**
+ * Provides factory methods for creating a RatPack {@link ParallelBatch}, with
+ * the correct trace context.
+ *
+ * This is necessary because {@link ParallelBatch}es create forked executions
+ * which do not "inherit" registries from the calling execution. Consequently,
+ * the trace context must be passed explicitly to the forked executions.
+ *
+ * @param <T> the type of value produced by each promise in the batch.
+ */
 public final class TracedParallelBatch<T> {
 
   private final Iterable<? extends Promise<T>> promises;
@@ -14,24 +24,66 @@ public final class TracedParallelBatch<T> {
     this.promises = promises;
   }
 
-  static <T> TracedParallelBatch<T> of(Iterable<? extends Promise<T>> promises) {
+  /**
+   * Create a {@link TracedParallelBatch} for list of {@link Promise}s.
+   *
+   * @param promises an iterable of Promises
+   * @param <T> the type of value produced by each promise
+   *
+   * @return an instance of {@link TracedParallelBatch}.
+   */
+  public static <T> TracedParallelBatch<T> of(Iterable<? extends Promise<T>> promises) {
     return new TracedParallelBatch<>(promises);
   }
 
-  static <T> TracedParallelBatch<T> of(Promise<T>... promise) {
-    return new TracedParallelBatch<>(Arrays.asList(promise));
+  /**
+   * Create a {@link TracedParallelBatch} for list of {@link Promise}s.
+   *
+   * @param promises vararg containing a list of {@link Promise}s.
+   * @param <T> the type of value produced by each promise
+   *
+   * @return an instance of {@link TracedParallelBatch}.
+   */
+  public static <T> TracedParallelBatch<T> of(Promise<T>... promises) {
+    return new TracedParallelBatch<>(Arrays.asList(promises));
   }
 
-  static <T> ParallelBatch<T> of(final TraceContext context, Iterable<? extends Promise<T>> promises) {
+  /**
+   * Create a {@link TracedParallelBatch} for list of {@link Promise}s,
+   * with the specified trace context.
+   *
+   * @param context the trace context
+   * @param promises an iterable of Promises
+   * @param <T> the type of value produced by each promise
+   *
+   * @return an instance of {@link ParallelBatch}.
+   */
+  public static <T> ParallelBatch<T> of(final TraceContext context, Iterable<? extends Promise<T>> promises) {
     return ParallelBatch.of(promises)
         .execInit(execution ->
             execution.add(new RatpackCurrentTraceContext.TraceContextHolder(context)));
   }
 
-  static <T> ParallelBatch<T> of(final TraceContext context, Promise<T>... promise) {
-    return of(context, Arrays.asList(promise));
+  /**
+   * Create a {@link TracedParallelBatch} for list of {@link Promise}s,
+   * with the specified trace context.
+   *
+   * @param context the trace context.
+   * @param promises vararg containing a list of {@link Promise}s.
+   * @param <T> the type of value produced by each promise
+   *
+   * @return an instance of {@link ParallelBatch}.
+   */
+  public static <T> ParallelBatch<T> of(final TraceContext context, Promise<T>... promises) {
+    return of(context, Arrays.asList(promises));
   }
 
+  /**
+   * Set the trace context.
+   *
+   * @param context the trace context.
+   * @return a ParallelBatch
+   */
   public ParallelBatch<T> withContext(final TraceContext context) {
     return of(context, promises);
   }
